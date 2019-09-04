ee1115c14765cc035b255bdea637edcffe739d1c
root <root@instance-2.asia-northeast2-a.c.majestic-choir-246323.internal>
2019-09-04 09:00:10 +0000

    setting completed

--- server/models.py
+++ server/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Place(models.Model):
+	id = models.IntegerField(primary_key=True)
+	
+	def __str__(self):
+		return self.id
--- server/views.py
+++ server/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+import json
+
+
+def get_number(request,number):
+
+	return HttpResponse(result,content_type=u"application/json; charset=utf-8")
